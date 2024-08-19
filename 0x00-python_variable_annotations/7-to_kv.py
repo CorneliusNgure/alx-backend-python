3d3cf88cf94d0d8c32a338d36162b496192b6895
CorneliusNgure <kingcornelius07@gmail.com>
2024-08-19 12:04:17 -0400

    Complex types - stirng and int/float to tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module for complex types - sting and int/float to tuple"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[int, float]:
+    """Complex types - string and int/float to tuple"""
+    return (k, v**2)
