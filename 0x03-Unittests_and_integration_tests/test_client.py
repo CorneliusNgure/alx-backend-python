6dc6db1a633fe78504a44d077104c4ac7af99eae
CorneliusNgure <kingcornelius07@gmail.com>
2025-09-20 02:12:26 -0700

    Patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -56,6 +56,39 @@ class TestGithubOrgClient(unittest.TestCase):
 
             self.assertEqual(result, expected_url)
 
+    @patch("client.get_json")
+    def test_public_repos(self, mock_get_json):
+        """
+        Ensure GithubOrgClient.public_repos returns the expected list.
+
+        - Ensure GithubOrgClient._public_repos_url is used,
+        - get_json is called once with the mocked URL, and
+        - the list of repo names matches the mocked payload.
+        """
+
+        mock_repos_payload = [
+            {"name": "repo1"},
+            {"name": "repo2"},
+            {"name": "repo3"},
+        ]
+        mock_get_json.return_value = mock_repos_payload
+
+        expected_url = "https://api.github.com/orgs/test/repos"
+
+        with patch.object(
+            GithubOrgClient,
+            "_public_repos_url",
+            new_callable=PropertyMock,
+            return_value=expected_url,
+        ) as mock_repos_url:
+            client = GithubOrgClient("test")
+            result = client.public_repos()
+
+            # Assert
+            self.assertEqual(result, ["repo1", "repo2", "repo3"])
+            mock_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with(expected_url)
+
 
 if __name__ == "__main__":
     unittest.main()
